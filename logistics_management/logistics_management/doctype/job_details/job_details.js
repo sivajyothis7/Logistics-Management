c7d8d96f2455afef2173198fe07f6d1e5c5e4165
sivajyothis7 <sivajyothis9446@gmail.com>
2024-05-12 02:03:05 +0530

    third

--- logistics_management/logistics_management/doctype/job_details/job_details.js
+++ logistics_management/logistics_management/doctype/job_details/job_details.js
@@ -1,24 +1,24 @@
 // Copyright (c) 2024, siva and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Job Details", {
-    refresh(frm) {
-        // Add a custom button "Generate POD"
-        frm.add_custom_button(__('Generate POD'), function() {
-            var objWindowOpenResult = window.open(frappe.urllib.get_full_url("/printview?"
-                + "doctype=" + encodeURIComponent("Job Details")
-                + "&name=" + encodeURIComponent(frm.doc.name)
-                + "&trigger_print=0"
-                + "&format=POD"
-                + "&no_letterhead=0"
-                + "&_lang=en"
-            ));
+// frappe.ui.form.on("Job Details", {
+//     refresh(frm) {
+//         // Add a custom button "Generate POD"
+//         frm.add_custom_button(__('Generate POD'), function() {
+//             var objWindowOpenResult = window.open(frappe.urllib.get_full_url("/printview?"
+//                 + "doctype=" + encodeURIComponent("Job Details")
+//                 + "&name=" + encodeURIComponent(frm.doc.name)
+//                 + "&trigger_print=0"
+//                 + "&format=POD"
+//                 + "&no_letterhead=0"
+//                 + "&_lang=en"
+//             ));
 
-            if (!objWindowOpenResult) {
-                frappe.msgprint(__("Please set permission for pop-up windows in your browser!"));
-                return;
-            }
-        });
-    },
-});
+//             if (!objWindowOpenResult) {
+//                 frappe.msgprint(__("Please set permission for pop-up windows in your browser!"));
+//                 return;
+//             }
+//         });
+//     },
+// });
 
