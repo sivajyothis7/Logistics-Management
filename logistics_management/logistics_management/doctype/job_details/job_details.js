179bf20f31bcb33c7250c2ced175546706b2079c
sivajyothis7 <sivajyothis9446@gmail.com>
2024-05-22 17:25:49 +0530

    third

--- logistics_management/logistics_management/doctype/job_details/job_details.js
+++ logistics_management/logistics_management/doctype/job_details/job_details.js
@@ -1,24 +1,81 @@
 // Copyright (c) 2024, siva and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Job Details", {
-//     refresh(frm) {
-//         // Add a custom button "Generate POD"
-//         frm.add_custom_button(__('Generate POD'), function() {
-//             var objWindowOpenResult = window.open(frappe.urllib.get_full_url("/printview?"
-//                 + "doctype=" + encodeURIComponent("Job Details")
-//                 + "&name=" + encodeURIComponent(frm.doc.name)
-//                 + "&trigger_print=0"
-//                 + "&format=POD"
-//                 + "&no_letterhead=0"
-//                 + "&_lang=en"
-//             ));
+frappe.ui.form.on("Job Details", {
+    refresh: function(frm) {
+        add_custom_button(frm);
+        frm.events.set_dashboard_indicators(frm); 
+    },
 
-//             if (!objWindowOpenResult) {
-//                 frappe.msgprint(__("Please set permission for pop-up windows in your browser!"));
-//                 return;
-//             }
-//         });
-//     },
-// });
+    set_dashboard_indicators: function(frm) {
+        function process_invoices(invoices, includeOutstanding = false) {
+            var totals = { grandTotal: 0, outstandingTotal: 0 };
+            if (invoices && invoices.length > 0) {
+                invoices.forEach(function(invoice) {
+                    totals.grandTotal += invoice.grand_total;
+                    if (includeOutstanding) {
+                        totals.outstandingTotal += invoice.outstanding_amount;
+                    }
+                });
+            }
+            return totals;
+        }
 
+        frappe.call({
+            method: 'frappe.client.get_list',
+            args: {
+                doctype: 'Sales Invoice',
+                filters: {
+                    custom_job_number: frm.doc.name,
+                    docstatus: 1
+                },
+                fields: ['grand_total', 'outstanding_amount']
+            },
+            callback: function(response) {
+                var salesInvoiceTotals = process_invoices(response.message, true);
+
+                frappe.call({
+                    method: 'frappe.client.get_list',
+                    args: {
+                        doctype: 'Purchase Invoice',
+                        filters: {
+                            custom_job_number: frm.doc.name,
+                            docstatus: 1
+                        },
+                        fields: ['grand_total', 'outstanding_amount']
+                    },
+                    callback: function(response) {
+                        var purchaseInvoiceTotals = process_invoices(response.message, true);
+
+                        var profitAndLoss = salesInvoiceTotals.grandTotal - purchaseInvoiceTotals.grandTotal;
+
+                        frm.dashboard.add_indicator(__('Total Sales Invoice: {0}', [format_currency(salesInvoiceTotals.grandTotal, frm.doc.currency)]), 'blue');
+                        frm.dashboard.add_indicator(__('Total Purchase Invoice: {0}', [format_currency(purchaseInvoiceTotals.grandTotal, frm.doc.currency)]), 'orange');
+                        // frm.dashboard.add_indicator(__('Total Sales Outstanding: {0}', [format_currency(salesInvoiceTotals.outstandingTotal, frm.doc.currency)]), salesInvoiceTotals.outstandingTotal ? 'red' : 'green');
+                        // frm.dashboard.add_indicator(__('Total Purchase Outstanding: {0}', [format_currency(purchaseInvoiceTotals.outstandingTotal, frm.doc.currency)]), purchaseInvoiceTotals.outstandingTotal ? 'red' : 'green');
+                        frm.dashboard.add_indicator(__('Profit & Loss: {0}', [format_currency(profitAndLoss, frm.doc.currency)]), profitAndLoss >= 0 ? 'green' : 'red');
+
+                    }
+                    
+                });
+            }
+        });
+    }
+});
+
+function add_custom_button(frm) {
+    frm.add_custom_button(__('Generate Project'), function() {
+        frappe.call({
+            method: 'logistics_management.logistics_management.doctype.job_details.job_details.generate_project',
+            args: {
+                docname: frm.docname
+            },
+            callback: function(response) {
+                if (response.message) {
+                    frm.reload_doc();
+                    // frappe.msgprint('Waybill generated successfully.');
+                }
+            }
+        });
+    });
+}
--- logistics_management/logistics_management/doctype/job_details/job_details.py
+++ logistics_management/logistics_management/doctype/job_details/job_details.py
@@ -1,9 +1,34 @@
 # Copyright (c) 2024, siva and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class JobDetails(Document):
 	pass
+
+@frappe.whitelist()
+def generate_project(docname):
+    if not docname:
+        frappe.msgprint("Document name is required to generate the waybill.")
+        return
+
+    doc = frappe.get_doc("Job Details", docname)
+
+    if doc.project:
+        frappe.throw(f"Project already exists for this Job: {doc.project}")
+    else:
+        data = frappe.get_doc({
+            "doctype": "Project",
+            "project_name": doc.name,
+            "custom_job_details": doc.name,
+            
+        })
+        data.insert(ignore_permissions=True)
+        frappe.msgprint(f"Project generated successfully: {data.name}")
+
+        doc.project = data.name
+        doc.save(ignore_permissions=True)
+
+        return doc.project
