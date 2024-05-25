882eec05e2764c446c5b8ba2934649836fab4df2
sivajyothis7 <sivajyothis9446@gmail.com>
2024-05-25 14:03:55 +0530

    third

---
+++ logistics_management/logistics_management/report/job_detail_metrics/job_detail_metrics.js
@@ -0,0 +1,48 @@
+frappe.query_reports["Job Detail Metrics"] = {
+    "filters": [
+        {
+            "fieldname": "job_details",
+            "label": __("Job Details"),
+            "fieldtype": "Link",
+            "options": "Job Details",
+            "reqd": 0,
+            "default": "",
+            "width": "80",
+            // "get_data": function(txt) {
+            //     return frappe.db.get_link_options('Job Details', txt);
+            // }
+        }
+    ],
+
+    "onload": function(report) {
+        report.page.add_inner_button(__('Refresh Data'), function() {
+            report.refresh();
+        });
+    },
+
+    "formatter": function(value, row, column, data, default_formatter) {
+        value = default_formatter(value, row, column, data);
+        if (column.fieldname == "profit_and_loss") {
+            if (data.profit_and_loss < 0) {
+                value = `<span style="color:red">${value}</span>`;
+            } else {
+                value = `<span style="color:green">${value}</span>`;
+            }
+        }
+        if (column.fieldname == "sales_outstanding_amount") {
+            if (flt(value) === 0) {
+                value = `<span style="color:black">${value}</span>`;
+            } else {
+                value = `<span style="color:green">${value}</span>`;
+            }
+        }
+        if (column.fieldname == "purchase_outstanding_amount") {
+            if (flt(value) === 0) {
+                value = `<span style="color:black">${value}</span>`;
+            } else {
+                value = `<span style="color:orange">${value}</span>`;
+            }
+        }
+        return value;
+    }
+};
---
+++ logistics_management/logistics_management/report/job_detail_metrics/job_detail_metrics.py
@@ -0,0 +1,148 @@
+import frappe
+from frappe.utils import flt
+
+def execute(filters=None):
+    columns, data = [], []
+
+    columns = [
+        {"label": "Job Details", "fieldname": "job_details", "fieldtype": "Link", "options": "Job Details", "width": 125},
+        {"label": "Invoice Type", "fieldname": "invoice_type", "fieldtype": "Data", "width": 135},
+        {"label": "Invoice ID", "fieldname": "invoice_id", "fieldtype": "Dynamic Link", "options": "invoice_type", "width": 195},
+        {"label": "Item", "fieldname": "item", "fieldtype": "Data", "width": 175},
+        {"label": "Quantity", "fieldname": "quantity", "fieldtype": "Float", "width": 100},
+        {"label": "Rate", "fieldname": "rate", "fieldtype": "Currency", "width": 100},
+        {"label": "Amount", "fieldname": "amount", "fieldtype": "Currency", "width": 150},
+        {"label": "Credit", "fieldname": "credit", "fieldtype": "Currency", "width": 150},
+        {"label": "Debit", "fieldname": "debit", "fieldtype": "Currency", "width": 150},
+        {"label": "Sales Outstanding", "fieldname": "sales_outstanding_amount", "fieldtype": "Currency", "width": 170},
+        {"label": "Purchase Outstanding", "fieldname": "purchase_outstanding_amount", "fieldtype": "Currency", "width": 175},
+        {"label": "Profit & Loss", "fieldname": "profit_and_loss", "fieldtype": "Currency", "width": 150},
+    ]
+
+    job_details_filter = filters.get('job_details') if filters else None
+
+    job_details_list = []
+    if job_details_filter:
+        job_details_list = frappe.get_all('Job Details', filters={'name': ['in', job_details_filter]}, fields=['name'])
+    else:
+        job_details_list = frappe.get_all('Job Details', fields=['name'])
+
+    overall_credit_total = 0
+    overall_debit_total = 0
+    overall_sales_outstanding_total = 0
+    overall_purchase_outstanding_total = 0
+    overall_profit_and_loss = 0
+
+    for job in job_details_list:
+        total_credit = 0
+        total_debit = 0
+        total_sales_outstanding = 0
+        total_purchase_outstanding = 0
+
+        # Fetch and process Sales Invoices
+        sales_invoices = frappe.get_all('Sales Invoice', filters={'custom_job_number': job.name, 'docstatus': 1}, fields=['name', 'base_grand_total', 'outstanding_amount'])
+        for inv in sales_invoices:
+            items = frappe.get_all('Sales Invoice Item', filters={'parent': inv.name}, fields=['item_code', 'qty', 'rate', 'amount'])
+            for item in items:
+                data.append({
+                    'job_details': job.name,
+                    'invoice_type': 'Sales Invoice',
+                    'invoice_id': inv.name,
+                    'item': item.item_code,
+                    'quantity': item.qty,
+                    'rate': item.rate,
+                    'amount': item.amount,
+                    'credit': flt(item.amount),
+                    'debit': 0,
+                    'sales_outstanding_amount': flt(inv.outstanding_amount),
+                    'purchase_outstanding_amount': 0,
+                    'profit_and_loss': ''
+                })
+            total_credit += flt(inv.base_grand_total)
+            total_sales_outstanding += flt(inv.outstanding_amount)
+
+        # Fetch and process Purchase Invoices
+        purchase_invoices = frappe.get_all('Purchase Invoice', filters={'custom_job_number': job.name, 'docstatus': 1}, fields=['name', 'base_grand_total', 'outstanding_amount'])
+        for inv in purchase_invoices:
+            items = frappe.get_all('Purchase Invoice Item', filters={'parent': inv.name}, fields=['item_code', 'qty', 'rate', 'amount'])
+            for item in items:
+                data.append({
+                    'job_details': job.name,
+                    'invoice_type': 'Purchase Invoice',
+                    'invoice_id': inv.name,
+                    'item': item.item_code,
+                    'quantity': item.qty,
+                    'rate': item.rate,
+                    'amount': item.amount,
+                    'credit': 0,
+                    'debit': flt(item.amount),
+                    'sales_outstanding_amount': 0,
+                    'purchase_outstanding_amount': flt(inv.outstanding_amount),
+                    'profit_and_loss': ''
+                })
+            total_debit += flt(inv.base_grand_total)
+            total_purchase_outstanding += flt(inv.outstanding_amount)
+
+        # Fetch and process Journal Entries (only debit items)
+        journal_entries = frappe.get_all('Journal Entry', filters={'custom_job_number': job.name, 'docstatus': 1}, fields=['name'])
+        for entry in journal_entries:
+            entries = frappe.get_all('Journal Entry Account', filters={'parent': entry.name, 'debit': ['>', 0]}, fields=['account', 'debit'])
+            for je in entries:
+                data.append({
+                    'job_details': job.name,
+                    'invoice_type': 'Journal Entry',
+                    'invoice_id': entry.name,
+                    'item': je.account,
+                    'quantity': '',
+                    'rate': '',
+                    'amount': je.debit,
+                    'credit': 0,
+                    'debit': flt(je.debit),
+                    'sales_outstanding_amount': '',
+                    'purchase_outstanding_amount': '',
+                    'profit_and_loss': ''
+                })
+                total_debit += flt(je.debit)
+
+        # Calculate profit and loss
+        profit_and_loss = total_credit - total_debit
+        overall_profit_and_loss += profit_and_loss
+
+        # Append totals for each job
+        data.append({
+            'job_details': job.name,
+            'invoice_type': '<b>Totals</b>',
+            'invoice_id': '',
+            'item': '',
+            'quantity': '',
+            'rate': '',
+            'amount': '',
+            'credit': total_credit,
+            'debit': total_debit,
+            'sales_outstanding_amount': total_sales_outstanding,
+            'purchase_outstanding_amount': total_purchase_outstanding,
+            'profit_and_loss': profit_and_loss
+        })
+
+        overall_credit_total += total_credit
+        overall_debit_total += total_debit
+        overall_sales_outstanding_total += total_sales_outstanding
+        overall_purchase_outstanding_total += total_purchase_outstanding
+
+    # Append overall totals
+    data.append({
+        'job_details': '',
+        'invoice_type': '<b>Overall Totals</b>',
+        'invoice_id': '',
+        'item': '',
+        'quantity': '',
+        'rate': '',
+        'amount': '',
+        'credit': overall_credit_total,
+        'debit': overall_debit_total,
+        'sales_outstanding_amount': overall_sales_outstanding_total,
+        'purchase_outstanding_amount': overall_purchase_outstanding_total,
+        'profit_and_loss': overall_profit_and_loss
+    })
+
+    return columns, data
